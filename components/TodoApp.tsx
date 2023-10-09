17188fa2d213044736e66b3c2f9834b22fc8e363
shang-shang95 <qqxn7ra9k@gmail.com>
2023-10-10 02:13:54 +0900

    feat: Add TodoApp component

---
+++ components/TodoApp.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TodoList from "./TodoList";
+
+const TodoApp = () => {
+  return (
+    <section className="text-center mb-2 text-2xl font-medium">
+      <h3>Supabase Todo App</h3>
+      <form>
+        <input type="text" className="mr-2 shadow-lg p-1 outline-none" />
+        <button className="shadow-md border-2 px-1 py-1 rounded-lg bg-green-400">
+          Add
+        </button>
+      </form>
+      <TodoList />
+    </section>
+  );
+};
+
+export default TodoApp;
