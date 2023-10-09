5c55db4c6260259d5cf02e3499f85d77622f6e0c
shang-shang95 <qqxn7ra9k@gmail.com>
2023-10-10 02:26:14 +0900

    style: Update TodoApp input styling

--- components/TodoApp.tsx
+++ components/TodoApp.tsx
@@ -4,9 +4,12 @@ import TodoList from "./TodoList";
 const TodoApp = () => {
   return (
     <section className="text-center mb-2 text-2xl font-medium">
-      <h3>Supabase Todo App</h3>
+      <h3 className="mb-4">Supabase Todo App</h3>
       <form>
-        <input type="text" className="mr-2 shadow-lg p-1 outline-none" />
+        <input
+          type="text"
+          className="mr-2 shadow-lg p-1 outline-none border-radius border-2"
+        />
         <button className="shadow-md border-2 px-1 py-1 rounded-lg bg-green-400">
           Add
         </button>
